7f2635652f2c2bbc940d868b9823c238d1c42f82
Joackim <jojo.blsegor@gmail.com>
2024-06-05 10:28:45 +0200

    Saves index.ts from models

--- models/index.ts
+++ models/index.ts
@@ -2,5 +2,3 @@ export * from './model.registry';
 export * from './salle.model';
 export * from './user.model';
 export * from './session.model';
-export * from './exercice.model';
-export * from './seance.model';
